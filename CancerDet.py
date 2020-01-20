788236f3db57662b3f821e8d449e557e629e11b3
maryambagherik <59444114+maryambagherik@users.noreply.github.com>
2020-01-20 13:09:47 +0100

    Add files via upload

---
+++ CancerDet.py
@@ -0,0 +1,79 @@
+# import the necessary packages
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import SeparableConv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+# reducing overfitting with Dropout
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras import backend as K
+
+class CancerDet:
+	# depth  is the number of color channels each image contains
+   # the number of classes our network will predict (0 and 1)
+	def build(width, height, depth, classes):
+		# initialize the model along with the input shape to be
+		# "channels last" and the channels dimension itself
+		model = Sequential()
+		inputShape = (height, width, depth)
+		chanDim = -1
+
+# channels_first  requires that we place the depth  at the front of the inputShape
+		# if we are using "channels first", update the input shape
+		# and channels dimension
+		if K.image_data_format() == "channels_first":
+			inputShape = (depth, height, width)
+			chanDim = 1
+
+## blocks are defined with increasing stacking and number of filters ##
+            
+		# DEPTHWISE_CONV => RELU => POOL
+		model.add(SeparableConv2D(32, (3, 3), padding="same",
+			input_shape=inputShape))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# (DEPTHWISE_CONV => RELU => POOL) * 2
+		model.add(SeparableConv2D(64, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(SeparableConv2D(64, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# (DEPTHWISE_CONV => RELU => POOL) * 3
+		model.add(SeparableConv2D(128, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(SeparableConv2D(128, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(SeparableConv2D(128, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization(axis=chanDim))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+
+## FC => RELU  layers and softmax classifier make the head of the network ##
+
+		# first (and only) set of FC => RELU layers
+		model.add(Flatten())
+		model.add(Dense(256))
+		model.add(Activation("relu"))
+		model.add(BatchNormalization())
+		model.add(Dropout(0.5))
+
+## output of the softmax classifier will be the prediction percentages 
+# for each class our model will predict
+		# softmax classifier
+		model.add(Dense(classes))
+		model.add(Activation("softmax"))
+
+		# return the constructed network architecture
+		return model
